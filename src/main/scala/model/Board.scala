4368d0e8382472e0904618d0c99885a2bc9a68c0
Kyle Annen <kyleannen@Kyles-MacBook-Pro.local>
2017-06-28 17:23:30 -0500

    finish board

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -3,6 +3,7 @@ package tictactoe
 object Board {
     val initialBoardState: List[Any] = List(1,2,3,4,5,6,7,8,9)
 
+    //returns the dimensions of the board
     def returnDimension(board: List[Any]): Int = {
         Math.sqrt(board.length).toInt
     }
@@ -11,7 +12,6 @@ object Board {
         val rowNum = returnDimension(board)
         board.grouped(rowNum).toList
     }
-    def checkRows() {}
     
     //returns a list of column lists
     def returnColumns(board: List[Any]): List[List[Any]] = {
@@ -19,11 +19,28 @@ object Board {
         val boardLen = board.length
         List.range(1, colNum + 1, 1).map(x => List.range(x, boardLen + 1, colNum))
     }
-    def checkColumns() {}
 
-    def returnDiagonals() = {
-
+    //returns a list of diagonal lists
+    def returnDiagonals(board: List[Any]): List[List[Any]] = {
+        val diaLen = returnDimension(board)
+        val boardLen = board.length
+        val dia1: List[Int] = List.range(1, boardLen + 1, diaLen + 1)
+        val dia2: List[Int] = List.range(diaLen, boardLen - 1, diaLen - 1)
+        List(dia1, dia2)
     }
-    def checkDiagnals() {}
+
+    //checks to see if a provided list of lists has a list with identical internal values
+    def checkSets(sets: List[List[Any]]): Boolean = {
+        val setBooleans = sets.map(set => set.forall(_ == set.head))
+        setBooleans.foldLeft(setBooleans(0))(_ || _)
+    }
+
+    //returns a boolean, checks diagonals, columns and rows for identical values
+    def checkWin(board: List[Any]): Boolean = {
+        val diaBool = checkSets(returnDiagonals(board))
+        val rowBool = checkSets(returnRows(board))
+        val colBool = checkSets(returnColumns(board))
+        diaBool || rowBool || colBool
+    }
 
 }
--- src/test/scala/model/BoardSpec.scala
+++ src/test/scala/model/BoardSpec.scala
@@ -31,4 +31,52 @@ class BoardSpec extends FunSpec {
 
         }
     }
+
+    describe("returnDiagonals") {
+        it("should return a list of diagonal lists") {
+            val testBoard: List[Any] = List(1,2,3,4,5,6,7,8,9)
+            val expected: List[List[Any]] = List(List(1,5,9), List(3,5,7))
+            val actual: List[List[Any]] = Board.returnDiagonals(testBoard)
+
+            assert( actual === expected )
+
+        }
+    }
+
+    describe("checkSets") {
+        it("should return true if one list in a list of lists has identical values internally") {
+            val testBoard: List[Any] = List("x","x","x",4,5,6,7,8,9)
+            val expected: Boolean = true
+            val actual = Board.checkSets(Board.returnRows(testBoard))
+
+            assert( actual === expected)
+        }
+        
+        it("should return return false if no list in a list of lists has identical values") {
+            val testBoard: List[Any] = List("x","x","x",4,5,6,7,8,9)
+            val expected: Boolean = false 
+            val actual = Board.checkSets(Board.returnColumns(testBoard))
+
+            assert( actual === expected)
+        }
+    }
+
+    describe("checkWin") {
+        it("should return true if a winning board") {
+            val testBoard: List[Any] = List("x","x","x",4,5,6,7,8,9)
+            val expected: Boolean = true
+            val actual = Board.checkWin(testBoard)
+
+            assert(actual === expected)
+        }
+
+        it("should return false if not a winning board") {
+            val testBoard: List[Any] = List(1,"x","x",4,5,6,7,8,9)
+            val expected: Boolean = false 
+            val actual = Board.checkWin(testBoard)
+
+            assert(actual === expected)
+
+        }
+    }
 }
