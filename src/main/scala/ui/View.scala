3a50313e0723798d87a141058117eaeb8763e4c1
Kyle Annen <kyleannen@Kyles-MacBook-Pro.local>
2017-06-28 09:48:00 -0500

    finish renderBoard

--- src/main/scala/ui/View.scala
+++ src/main/scala/ui/View.scala
@@ -1,27 +1,37 @@
-package view 
+package tictactoe 
 
 object View extends App {
 
-  val greeting: String = "Welcome to TicTacToe"
+  val board = List(1,2,3,4,5,6,7,8)
 
-  val moves = List.fill(9)(null)
-
-  def render(x: String) {
-    println(x)
+  def formatRow(row: List[Any]): String = {
+    val struct: String = " %d | %d | %d "
+    struct.format(row(0), row(1), row(2))
   }
 
-  def createBoard(moves: List ) {
-    moves.flatMap(move => if())
-
-
+  def renderWhitespace(n: Int): Unit = {
+    for( i <- 1 to 10 ) {
+      println()
+    }
   }
 
-  def renderBoard(board: List) {
+  def renderBoard(board: List[Any]): Unit = {
+    val row1 = board.slice(0,3)
+    val row2 = board.slice(2,6)
+    val row3 = board.slice(5,9)
+    val hLine = "===+===+==="
 
-
+    renderWhitespace(10)
+    println(formatRow(row1))
+    println(hLine)
+    println(formatRow(row2))
+    println(hLine)
+    println(formatRow(row3))
+    renderWhitespace(10)
   }
 
-  render(greeting)
+  
+  renderBoard(board)
 }
 
 
