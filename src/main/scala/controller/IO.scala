9b75f304cb794c196d5aa2e6e7a6e69a1095846a
Kyle Annen <kyleannen@Kyles-MacBook-Pro.local>
2017-07-03 16:56:22 -0500

    add travisci

--- src/main/scala/controller/IO.scala
+++ src/main/scala/controller/IO.scala
@@ -2,45 +2,36 @@ package tictactoe
 //IO handles the user input
 object IO {
 
+  //callCount agrument used purely for mocking / testing
   def getInput(callCount: Int = 0): String = {
     scala.io.StdIn.readLine().trim
   }
 
   def getUserInput(getInput: Int => String): String = {
-    val userInput = getInput(0).trim 
-      userInput
-    }
-  //prompts the user input, validates against values, reprompts if incorrect input,
-  //and returns the value
-    def getValidMove(
-      validValues: List[String], 
-      inputPrompt: String, 
-      invalidPlay: String,
-      output: String => Any,
-      getInput: Int => String,
-      leftPadding: Int) =  {
-      
-      def go(validity: Boolean, input: String, callCount: Int = 0): String  = {
-
-        if(validity) {
-          input
-        } else {
-          View.renderDialog(output,leftPadding,inputPrompt)
-          val input: String = getInput(callCount) 
-          try {
-            val inputValidity = validValues.contains(input)
-            val newCallCount = callCount + 1
-            go(inputValidity, input, newCallCount)
-          } catch {
-            case _: Throwable => {
-            View.renderDialog(output,leftPadding,invalidPlay)
-            val nextCallCount = callCount + 1
-            go(false, input, nextCallCount)
-          }
-        }
+  val userInput = getInput(0).trim 
+    userInput
+  }
+  
+  def getValidMove(
+    validValues: List[String], 
+    inputPrompt: String, 
+    invalidPlay: String,
+    output: String => Any,
+    getInput: Int => String,
+    leftPadding: Int) =  {
+    
+    def go(input: String, callCount: Int = 0): String  = {
+      View.renderDialog(output, leftPadding, inputPrompt)
+      val input: String = getInput(callCount)
+      val inputValidity = validValues.contains(input)
+      if (inputValidity) {
+        input
+      } else {
+        View.renderDialog(output, leftPadding, invalidPlay)
+        go(input, callCount + 1)
       }
     }
-
-  go(false, "none", 0)
+    go("none", 0)
   }
+
 }
