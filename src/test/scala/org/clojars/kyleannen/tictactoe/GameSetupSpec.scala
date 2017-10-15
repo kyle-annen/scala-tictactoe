f9bea90c9b237737924dc8e1348f0e9195389642
Kyle Annen <kyleannen@gmail.com>
2017-10-15 12:51:22 -0500

    clean up tests

--- src/test/scala/org/clojars/kyleannen/tictactoe/GameSetupSpec.scala
+++ src/test/scala/org/clojars/kyleannen/tictactoe/GameSetupSpec.scala
@@ -63,4 +63,56 @@ class GameSetupSpec extends FunSpec{
       assert(actual == expected)
     }
   }
+
+describe("getDifficulty") {
+  describe("3x3 board") {
+      val board3x3 = Board.initBoard(9)
+      it("returns depth for 3x3 board easy difficulty") {
+        val actual = GameSetup.getDifficulty(board3x3, 1)
+        val expected = 1
+        assert(expected == actual)
+      }
+
+      it("returns depth for 3x3 board moderate difficulty") {
+        val actual = GameSetup.getDifficulty(board3x3, 2)
+        val expected = 4
+        assert(expected == actual)
+      }
+
+      it("returns depth for 3x3 board hard difficulty") {
+        val actual = GameSetup.getDifficulty(board3x3, 3)
+        val expected = 10
+        assert(expected == actual)
+      }
+    }
+  }
+
+  describe("4x4 board") {
+    val board4x4 = Board.initBoard(16)
+    it("returns depth for 4x4 board easy difficulty") {
+      val actual = GameSetup.getDifficulty(board4x4, 1)
+      val expected = 1
+      assert(expected == actual)
+    }
+
+    it("returns depth for 4x4 board moderate difficulty") {
+      val actual = GameSetup.getDifficulty(board4x4, 2)
+      val expected = 2
+      assert(expected == actual)
+    }
+
+    it("returns depth for 4x4 board hard difficulty early game") {
+      val testBoard = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16".split(",").toList
+      val actual = GameSetup.getDifficulty(testBoard, 3)
+      val expected = 2
+      assert(expected == actual)
+    }
+
+    it("returns depth for 4x4 board hard difficulty late game") {
+      val testBoard = "X,X,X,O,O,O,X,O,X,10,11,12,13,14,O,X".split(",").toList
+      val actual = GameSetup.getDifficulty(testBoard, 3)
+      val expected = 6
+      assert(expected == actual)
+    }
+  }
 }
